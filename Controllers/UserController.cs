e65ac948af96275f0f0eab1753130669216e9fd4
YeisonGomez <yeigomez97@hotmail.com>
2017-03-28 14:18:30 -0500

    add util

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -8,17 +8,25 @@ namespace road
 	{
 
 		UserModel userModel = new UserModel();
+		Response json = new Response();
 
-		[Route("login2")]
+		[Route("login")]
 		[HttpPost]
 		public IHttpActionResult Login([FromBody] UserModel user)
 		{
-			return Json(userModel.Login(user.email, user.password));
+			if (user != null && user.email != null && user.password != null && user.email.Length <= 100 && user.password.Length <= 100)
+			{
+				return Json(userModel.Login(user.email, user.password));
+			}
+			else
+			{
+				return Json(json);
+			}
 		}
 
 		[Route("login2")]
 		[HttpGet]
-		public IHttpActionResult Login2()
+		public IHttpActionResult Test()
 		{
 			return Json("hola");
 		}
---
+++ Util/Response.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace road
+{
+	public class Response { 
+		public String status { get; set; }
+		public String type { get; set; }
+		public String description { get; set; }
+
+		public Response() {
+			this.clearJSON();
+		}
+
+		public void clearJSON() { 
+			this.status = "ERROR";
+			this.type = "error_null";
+			this.description = "No se han suministrado datos.";
+		}
+	}
+	
+}

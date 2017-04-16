b01eaf807ace7809bc882a7c9e46deb3aaa0f0f8
YeisonGomez <yeigomez97@hotmail.com>
2017-04-16 05:42:47 -0500

    (reserve): new module

---
+++ road/Controllers/ReserveController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace road.Controllers
+{
+	[RoutePrefix("api/reserve")]
+    public class ReserveController : ApiController
+    {
+		Response json = new Response();
+		UserModel userModel = new UserModel();
+		ReserveModel reserveModel = new ReserveModel();
+		Util util = new Util();
+
+		[Route("pre")]
+		[HttpPost]
+		public IHttpActionResult PreReserve()
+		{
+			Object[] auth = util.Authorization();
+			if ((String)auth[0] == "OK")
+			{
+				UserModel payload = (UserModel)auth[1];
+				return Json(userModel.GetRols(payload.email));
+			}
+			else
+			{
+				return Json(json.MysqlException((String)auth[1], (String)auth[2]));
+			}
+		}
+    }
+}
---
+++ road/Models/ReserveModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+namespace road
+{
+	public class ReserveModel
+	{
+		Connection con = new Connection();
+
+		public DataTable GetAll(String empresa_id)
+		{
+			String[] keys = { "p_empresa_id" };
+			String[] values = { empresa_id };
+			DataTable response = con.RunProcedure("GET_SUCURSALES", keys, values);
+			return response;
+		}
+	}
+}

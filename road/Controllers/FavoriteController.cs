5dfdda3a3e6a15c0d36546b425d4829b1bce8a00
YeisonGomez <yeigomez97@hotmail.com>
2017-05-16 02:21:41 -0500

    feat(favorite): new module

---
+++ road/Controllers/FavoriteController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace road.Controllers
+{
+	[RoutePrefix("api/favorite")]
+    public class FavoriteController : ApiController
+    {
+		FavoriteModel favoriteModel = new FavoriteModel();
+		Response json = new Response();
+		Util util = new Util();
+
+		[Route("user-add")]
+		[HttpPost]
+		public IHttpActionResult AddFavorite([FromBody] ReserveModel vehicle)
+		{	
+			Object[] auth = util.Authorization();
+			if ((String)auth[0] == "OK")
+			{
+				UserModel payload = (UserModel)auth[1];
+				if (vehicle.vehiculo_id != null)
+				{
+					return Json(favoriteModel.AddFavorite(payload.id.ToString(), vehicle.vehiculo_id));
+				}
+				else
+				{
+					return Json(json);
+				}
+			}
+			else
+			{
+				return Json(json.MysqlException((String)auth[1], (String)auth[2]));
+			}
+		}
+
+		/*[Route("permissions")]
+		[HttpGet]
+		public IHttpActionResult Permissions()
+		{
+			Object[] auth = util.Authorization();
+			if ((String)auth[0] == "OK")
+			{
+				UserModel payload = (UserModel)auth[1];
+				return Json(userModel.GetPermissions(payload.email));
+			}
+			else
+			{
+				return Json(json.MysqlException((String)auth[1], (String)auth[2]));
+			}
+		}*/
+    }
+}
---
+++ road/Models/FavoriteModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace road
+{
+	public class FavoriteModel
+	{
+		public Response response = new Response();
+		Connection con = new Connection();
+
+		public DataTable AddFavorite(string user_id, string vehicle_id)
+		{
+			try
+			{
+				String[] keys = { "p_user_id", "p_vehicle_id" };
+				String[] values = { user_id, vehicle_id };
+				DataTable response = con.RunProcedure("ADD_FAVORITE", keys, values);
+				return response;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				return null;
+			}
+		}
+		public DataTable GetUserReserve(String email)
+		{
+			String[] keys = { "p_email" };
+			String[] values = { email };
+			DataTable respons = con.RunProcedure("GET_USER_RESERVE", keys, values);
+			return respons;
+		}
+	}
+}
